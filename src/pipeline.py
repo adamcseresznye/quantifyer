40d28fb7c6a574e4734bbda68bc66525fa90f39b
adamcseresznye <csenye.chem@gmail.com>
2024-01-11 15:50:12 +0100

    modules reviewed and plotting added to pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,7 +1,9 @@
-import concentration_calculator
-import data
-import qc
-import recovery
+"""A module for executing the dataprpocessing pipeline.
+
+This module provides a simple interface for executing the dataprocessing pipeline.
+It returns recovery, correction factors and concentrations.
+"""
+from functools import partial
 
 
 class Pipeline:
@@ -18,25 +20,43 @@ class Pipeline:
         self.recovery_calculator = recovery_calculator
         self.correction_factor_calculator = correction_factor_calculator
         self.concentration_calculator = concentration_calculator
+        default_args = {
+            "by_sample": True,
+        }
+        self.plot_functions = {
+            "recovery": partial(self.recovery_calculator.plot_recovery, **default_args),
+            "response_factor": partial(
+                self.recovery_calculator.plot_response_factor, **default_args
+            ),
+            "correction_factor": self.correction_factor_calculator.plot_correction_factor,
+            "concentration": partial(
+                self.concentration_calculator.plot_concentration, **default_args
+            ),
+        }
 
     def execute(self):
         # Step 1: Validate the data
         self.data_validator.validate_data()
 
-        # Initialize recovery as None
+        # Initialize recovery as None and return if no is_concentration_file provided
         recovery = None
         if self.data.is_concentration_file is not None:
             # Step 2: Calculate recovery
             recovery = self.recovery_calculator.calculate_recovery()
 
-        correction_factors = None
-        if self.data.qc_file is not None:
-            # Step 3: Calculate correction factors
-            correction_factors = (
-                self.correction_factor_calculator.calculate_correction_factor()
-            )
+        # Step 3: Calculate correction factors
+        correction_factors = (
+            self.correction_factor_calculator.calculate_correction_factor()
+        )
 
         # Step 4: Calculate concentrations
         concentrations = self.concentration_calculator.calculate_concentration()
 
         return recovery, correction_factors, concentrations
+
+    def display_plot(self, plot_name, **kwargs):
+        plot_function = self.plot_functions.get(plot_name)
+        if plot_function:
+            plot_function(**kwargs)
+        else:
+            print(f"No plot function found for {plot_name}.")
--- src/qc.py
+++ src/qc.py
@@ -6,7 +6,6 @@ known amounts of analytes with their experimentally measured values.
 """
 
 import matplotlib.pylab as plt
-import numpy as np
 import pandas as pd
 
 import common_operations
@@ -17,23 +16,27 @@ class CorrectionFactor(common_operations.BaseCalculator):
         super().__init__(data)
 
     def calculate_measured_qc_concentration(self):
-        CONVERT_TO_NGML = 1000
-        native_concentration_in_qc = self.get_sample_concentrations_by_sample_type("qc")
-        AVG_native_concentration_in_blank = (
-            self.get_sample_concentrations_by_sample_type("blank")
-        ).mean(axis="columns")
-
-        blank_substracted_native_concentration_in_qc = (
-            native_concentration_in_qc.sub(
-                AVG_native_concentration_in_blank, axis="index"
+        # if there are no qc files in the sample_properties_file return None
+        if len(self.get_sample_names_by_sample_type("qc")) != 0:
+            CONVERT_TO_NGML = 1000
+            native_concentration_in_qc = self.get_sample_concentrations_by_sample_type(
+                "qc"
             )
-            .div(self.get_sample_volume_by_sample_type("qc"), axis=1)
-            .div(CONVERT_TO_NGML)
-        )
-        return blank_substracted_native_concentration_in_qc
+            AVG_native_concentration_in_blank = (
+                self.get_sample_concentrations_by_sample_type("blank")
+            ).mean(axis="columns")
+
+            blank_substracted_native_concentration_in_qc = (
+                native_concentration_in_qc.sub(
+                    AVG_native_concentration_in_blank, axis="index"
+                )
+                .div(self.get_sample_volume_by_sample_type("qc"), axis=1)
+                .div(CONVERT_TO_NGML)
+            )
+            return blank_substracted_native_concentration_in_qc
 
     def calculate_correction_factor(self):
-        if self.data.is_concentration_file is None:
+        if self.data.qc_file is None:
             # if the qc file is not present, return a correction factor set to 1 for all analytes
             unique_names = pd.Series(self.data.quant_file.name.unique())
             isrs_name = [x for name in self.get_is_rs_name().values() for x in name]
@@ -42,7 +45,7 @@ class CorrectionFactor(common_operations.BaseCalculator):
                     "native": unique_names[~unique_names.isin(isrs_name)].reset_index(
                         drop=True
                     ),
-                    "concentration": 1.0,
+                    "concentration": 1,
                 }
             )
             return correction_factor_set_to_one.set_index("native").squeeze()
